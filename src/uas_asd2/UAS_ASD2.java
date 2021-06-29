d09dfbddc37a66bc9b5bfc2b25cfa9a6c8137c06
muhfaishalrizal <muhammadfaishalrizal27@gmail.com>
2021-06-29 09:11:33 +0700

    UAs

---
+++ src/uas_asd2/UAS_ASD2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas_asd2;
+
+/**
+ *
+ * @author faishalrizal
+ */
+public class UAS_ASD2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
